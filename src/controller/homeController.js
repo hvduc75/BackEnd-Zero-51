484abe77b9fe44bbf7d1243f0733419c055b0046
Hoang Van Duc <duch52362@gmail.com>
2024-03-15 21:51:56 +0700

    video 12

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -13,6 +13,15 @@ let getDetailPage = async (req, res) => {
     return res.send(JSON.stringify(user));
 }
 
+let createNewUser = async (req, res) => {
+  let {firstName, lastName, email, address} = req.body;
+  await pool.execute('INSERT INTO users(firstName, lastName, email, address) values (?, ?, ?, ? )', 
+  [firstName, lastName, email, address]);
+  return res.redirect('/');
+}
+
 module.exports = {
-  getHomePage, getDetailPage
+  getHomePage, getDetailPage, createNewUser
 };
+
+
--- src/route/web.js
+++ src/route/web.js
@@ -4,7 +4,8 @@ let router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomePage);
-  router.get('/detail/user/:id', homeController.getDetailPage)
+  router.get('/detail/user/:id', homeController.getDetailPage);
+  router.post('/create-new-user', homeController.createNewUser);
   router.get("/about", (req, res) => {
     res.send('hello world');
   });
