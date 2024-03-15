48f3239f2c10023ed7a5e176b21bd9b8cb6ba8bb
Hoang Van Duc <duch52362@gmail.com>
2024-03-15 21:16:21 +0700

    video 11

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,22 +1,18 @@
-import connection from "../configs/connectDB";
+import pool from "../configs/connectDB";
 
-let getHomePage = (req, res) => {
-  // logic at here
-  let data = [];
-  connection.query("SELECT * FROM `users`", function (err, results, fields) {
-    results.map((row) => {
-      data.push({
-        id: row.id,
-        email: row.email,
-        address: row.address,
-        lastName: row.lastName,
-        firstName: row.firstName,
-      });
-    });
-    return res.render("index.ejs", { dataUser: data });
-  });
+let getHomePage = async (req, res) => {
+
+  const [rows, fields] = await pool.execute("SELECT * FROM users");
+
+  return res.render("index.ejs", { dataUser: rows });
 };
 
+let getDetailPage = async (req, res) => {
+    let userId = req.params.id;
+    let [user, fields] = await pool.execute(`SELECT * FROM users WHERE id = ?`, [userId]);
+    return res.send(JSON.stringify(user));
+}
+
 module.exports = {
-  getHomePage,
+  getHomePage, getDetailPage
 };
