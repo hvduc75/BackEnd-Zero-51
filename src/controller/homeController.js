2fe364f8c3cbb1603eacb0b62cf2a3e700e8aa04
Hoang Van Duc <duch52362@gmail.com>
2024-03-15 17:04:26 +0700

    video 8

---
+++ src/controller/homeController.js
@@ -0,0 +1,9 @@
+
+let getHomePage = (req, res) => {
+    // logic at here
+    return res.render("index.ejs");
+}
+
+module.exports = {
+    getHomePage
+}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web';
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-configViewEngine(app);
-app.get("/", (req, res) => {
-  res.render('index.ejs')
-});
+configViewEngine(app); // setup view engine
+initWebRoute(app); // init web route
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
